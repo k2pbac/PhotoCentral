e51d30e299941a6714a373771178002a97b02d12
Kris <k2bachan@ryerson.ca>
2021-06-09 16:39:43 -0400

    refactored app.js

--- app.js
+++ app.js
@@ -126,7 +126,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
+    res.locals.path = req.path;
+    res.locals.user = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
