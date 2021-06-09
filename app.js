28172824bcc041768c9b5a7dda11475767274c9a
Kris <k2bachan@ryerson.ca>
2021-06-09 16:07:02 -0400

    Refactored app.js

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+    require('dotenv').config();
 }
 
-const express = require("express");
-const helmet = require("helmet");
-const mongoose = require("mongoose");
-const path = require("path");
-const ejsMate = require("ejs-mate");
-const methodOverride = require("method-override");
-const ExpressError = require("./utils/ExpressError");
-const session = require("express-session");
-const flash = require("connect-flash");
-const mongoSanitize = require("express-mongo-sanitize");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const userRoutes = require('./routes/users');
+const photoRoutes = require('./routes/photos');
+const reviewRoutes = require('./routes/reviews');
 
+const MongoDBStore = require("connect-mongo")(session);
 
-//User Authentication Passport
-const passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    User = require("./models/user");
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/PhotoPortfolio';
 
-
-const app = express();
-const MongoDBStore = require("connect-mongo");
-
-
-const photosRoutes = require("./routes/photos");
-const reviewsRoutes = require("./routes/reviews");
-const usersRoutes = require("./routes/users");
-
-// const dbURL = process.env.DB_URL;
-const db_URL = process.env.DB_URL || "mongodb://localhost:27017/PhotoPortfolio";
-
-mongoose.connect(db_URL, {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
-})
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -44,38 +36,33 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-//setting view engine, ejs and paths
-app.set("view engine", "ejs");
-app.engine("ejs", ejsMate);
-app.set("views", path.join(__dirname, "views"));
+const app = express();
 
-//getting info from forms 
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-//using _method string to use delete/patch 
-app.use(methodOverride("_method"));
-
-//serve static files via public folder
-app.use(express.static(path.join(__dirname, "public")))
-app.use(mongoSanitize());
-
-// Advanced usage
-const store = MongoDBStore.create({
-    mongoUrl: db_URL,
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
     touchAfter: 24 * 60 * 60
 });
 
-store.on("error", function (err) {
-    console.log("Session store error", e);
-});
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
-
-const secret = process.env.SECRET || "secretpassword";
-
-//Session configuration
 const sessionConfig = {
     store,
-    name: 'fafweagzczvcvs',
+    name: 'session',
     secret,
     resave: false,
     saveUninitialized: true,
@@ -86,9 +73,11 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet());
+
 
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
@@ -105,7 +94,6 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
@@ -136,45 +124,42 @@ app.use(
 );
 
 
-//Passport configuration
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.path = req.path || "/";
-    res.locals.user = req.user || "";
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/photos', photoRoutes)
+app.use('/photos/:id/reviews', reviewRoutes)
+
+
+app.get('/', (req, res) => {
+    res.render('home')
 });
 
 
-//using external route files
-app.use("/photos", photosRoutes);
-app.use("/photos/:id/reviews", reviewsRoutes);
-app.use("/", usersRoutes);
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
 
-
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-
-//route for page not found
-app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found", 404));
-});
-
-//Error handling
 app.use((err, req, res, next) => {
-    const { status = 500, message = "Something went wrong!" } = err;
-    res.status(status).render("error", { message, status, err });
-});
-const port = process.env.PORT || 3000;
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
-app.listen(port, (req, res) => {
-    console.log("Listening on Port ", port);
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
